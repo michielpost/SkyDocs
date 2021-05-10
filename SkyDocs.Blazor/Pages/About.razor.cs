7250df275bcc4fafa6b3122a50f17f10add21e63
michielpost <michiel@q42.nl>
2021-05-10 19:53:41 +0200

    Show version

---
+++ SkyDocs.Blazor/Pages/About.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SkyDocs.Blazor.Pages
+{
+    public partial class About
+    {
+        private string? version;
+
+        public About()
+        {
+            version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        }
+    }
+}
