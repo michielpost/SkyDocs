f3b18810a5eff91bd0d13be1bac7343ec2abf99c
michielpost <michiel@michielpost.nl>
2022-11-11 08:52:53 +0000

    Deploying to gh-pages from  @ 21f8858fdc2e4928b93c4b69298cb8dfe9f2573f 🚀

---
+++ js/interop.js
@@ -0,0 +1,21 @@
+﻿//DrawImage
+window.Snap = async (src, format) => {
+    let rte = document.getElementById(src).children[1];
+
+    let canvas = await html2canvas(rte, {
+        scale: 0.3,
+        width: 910,
+        height: 500});
+    let dataUrl = canvas.toDataURL(format);
+    return dataUrl.split(',')[1];
+}
+
+window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
